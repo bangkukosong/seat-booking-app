a448081dda3a655f923b5b0be19df03f28e6c03e
bangkukosong <bautkendor2025@gmail.com>
2025-11-14 23:54:26 +0700

    Refactor app.js for improved readability

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // js/app.js
-
+let currentUser = null; 
 // ==================== CORE APP VARIABLES ====================
 const API_URL = "https://script.google.com/macros/s/AKfycbzqcu9ZSgpR5iGm4dMKC-S6GeG4lbYlsxMt_kBJBeHW7vEYcJpJHh1-CsunO5uOsYt6YQ/exec";
 const CACHE_DURATION = 120000;
@@ -80,4 +80,5 @@ function refreshBookings() {
   loadBookings();
   loadHistoricalBookings();
   showMessage("🔄 Data Refreshed!", "success");
+
 }
